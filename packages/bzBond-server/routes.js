e78a3e47d2f1ba7a0bd543d041c1aa8da5cf202f
Federico Ramirez <federico_r@beezwax.net>
2022-11-14 11:50:27 -0300

    Prettier

--- packages/bzBond-server/routes.js
+++ packages/bzBond-server/routes.js
@@ -5,56 +5,68 @@ const MAX_TIMEOUT = 45000;
 const BODY_LIMIT = 104857600;
 
 const functionBodyJsonSchema = {
-  type: 'object',
-  required: ['func'],
+  type: "object",
+  required: ["func"],
   properties: {
-    func: { type: 'string', minLength: 1 },
-    arguments: { type: 'array' },
-    timeout: { type: 'number' }
-  }
+    func: { type: "string", minLength: 1 },
+    arguments: { type: "array" },
+    timeout: { type: "number" },
+  },
 };
 
 const functionSchema = {
-  body: functionBodyJsonSchema
+  body: functionBodyJsonSchema,
 };
 
 const codeBodyJsonSchema = {
-  type: 'object',
-  required: ['jsCode'],
+  type: "object",
+  required: ["jsCode"],
   properties: {
-    jsCode: { type: 'string', minLength: 1 },
-    timeout: { type: 'number' }
-  }
+    jsCode: { type: "string", minLength: 1 },
+    timeout: { type: "number" },
+  },
 };
 
 const codeSchema = {
-  body: codeBodyJsonSchema
+  body: codeBodyJsonSchema,
 };
 
 async function routes(fastify, options) {
-  fastify.post('/function', { schema: functionSchema, bodyLimit: BODY_LIMIT }, (request, reply) => {
-    const func = request.body.func;
-    const timeout = request.body.timeout ? Math.min(MAX_TIMEOUT, request.body.timeout) : MAX_TIMEOUT;
-    const argumentString = JSON.stringify(request.body.arguments);
-    const vm = new VM({
-      timeout,
-      allowAsync: false,
-      sandbox: {
-        bzBond,
-        func
-      }
-    });
-    return vm.run(`bzBond(func, ...${argumentString})`);
-  });
+  fastify.post(
+    "/function",
+    { schema: functionSchema, bodyLimit: BODY_LIMIT },
+    (request, reply) => {
+      const func = request.body.func;
+      const timeout = request.body.timeout
+        ? Math.min(MAX_TIMEOUT, request.body.timeout)
+        : MAX_TIMEOUT;
+      const argumentString = JSON.stringify(request.body.arguments);
+      const vm = new VM({
+        timeout,
+        allowAsync: false,
+        sandbox: {
+          bzBond,
+          func,
+        },
+      });
+      return vm.run(`bzBond(func, ...${argumentString})`);
+    }
+  );
 
-  fastify.post('/code', { schema: codeSchema, bodyLimit: BODY_LIMIT }, (request, reply) => {
-    const timeout = request.body.timeout ? Math.min(MAX_TIMEOUT, request.body.timeout) : MAX_TIMEOUT;
-    const vm = new VM({
-      timeout,
-      allowAsync: false
-    });
-    return vm.run(request.body.jsCode);
-  });
+  fastify.post(
+    "/code",
+    { schema: codeSchema, bodyLimit: BODY_LIMIT },
+    (request, reply) => {
+      const timeout = request.body.timeout
+        ? Math.min(MAX_TIMEOUT, request.body.timeout)
+        : MAX_TIMEOUT;
+      const vm = new VM({
+        timeout,
+        allowAsync: false,
+      });
+      return vm.run(request.body.jsCode);
+    }
+  );
 }
 
 export default routes;
--- packages/bzBond-server/server.js
+++ packages/bzBond-server/server.js
@@ -4,24 +4,20 @@ import plugins from "./plugins.js";
 
 // Run the server!
 const start = async () => {
-
-  const installedPlugins = await plugins();
-
   const fastify = Fastify({
-    logger: true
+    logger: true,
   });
 
-  installedPlugins.forEach(plugin =>
-    fastify.register(plugin.default)
-  );
+  const installedPlugins = await plugins();
+  installedPlugins.forEach((plugin) => fastify.register(plugin.default));
 
   fastify.register(routes);
 
   try {
-    await fastify.listen(8999)
+    await fastify.listen(8999);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
 };
 start();
