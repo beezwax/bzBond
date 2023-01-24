64a37a7fe09898c5df9dcfc4cf8cb053c59650b0
Alec Gregory <alec_g@beezwax.net>
2023-01-24 14:39:07 +0000

    Clean dist file after build

--- packages/bzBond-web-template/webpack.prod.js
+++ packages/bzBond-web-template/webpack.prod.js
@@ -1,9 +1,20 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const InlineChunkHtmlPlugin = require('./plugins/inline-chunk-html-plugin')
+const InlineChunkHtmlPlugin = require('./plugins/inline-chunk-html-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "production",
-  plugins: [new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/app.bundle/])],
+  plugins: [
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/app.bundle/]),
+    new RemovePlugin({
+      after: {
+        root: './dist',
+        include: [
+            'app.bundle.js'
+        ]
+      }
+    })
+  ],
 });
