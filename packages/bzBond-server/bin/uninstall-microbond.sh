257d1aac44d28bbf237ed6bfbc591753165ca451
Alec Gregory <alec_g@beezwax.net>
2023-03-24 10:43:44 +0000

    Fix uninstall-microbond macOS

--- packages/bzBond-server/bin/uninstall-microbond.sh
+++ packages/bzBond-server/bin/uninstall-microbond.sh
@@ -4,5 +4,11 @@ echo "======================================"
 echo "bzBond Server Microbond Uninstallation"
 echo "======================================"
 
+# Check FileMaker's node
+NODE_PATH="/opt/FileMaker/FileMaker Server/node/bin/node"
+if [ "$(uname)" = "Darwin" ]; then
+  NODE_PATH="/Library/FileMaker Server/node/bin/node"
+fi
+
 # Run uninstall script
 sudo "$NODE_PATH" /var/www/bzbond-server/scripts/uninstall-microbond.js "$1"
