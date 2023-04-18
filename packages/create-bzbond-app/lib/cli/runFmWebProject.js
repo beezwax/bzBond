241ff0cb44502c182072001ba8a82275f6f1048a
Alec Gregory <alec_g@beezwax.net>
2023-04-18 09:19:53 +0100

    Fix script name

--- packages/create-bzbond-app/lib/cli/runFmWebProject.js
+++ packages/create-bzbond-app/lib/cli/runFmWebProject.js
@@ -1,7 +1,7 @@
 const performFmUrlScript = require("./performFmUrlScript");
 
 module.exports = (repoName, port) => {
-  const script = "FMBond Open Default Web Viewer In Debug Mode";
+  const script = "bzBond Open Default Web Viewer In Debug Mode";
   const debugUrl = `http://localhost:${port}`;
   const scriptParameter = {webProject: repoName, debugUrl};
   performFmUrlScript(script, scriptParameter, repoName);
